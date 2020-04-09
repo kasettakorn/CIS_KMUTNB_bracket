16cf57a23cd4cf58246372c7b6f8a5d5ca7a3f60
kasettakorn <kasettakorn_techno@hotmail.com>
2020-04-09 22:40:15 +0700

    Updates

---
+++ precache-manifest.ab439b4d9c2be504e8550373a1969780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5c27aa8e27549fd278257a92c6b196",
+    "url": "/CIS_KMUTNB_bracket/index.html"
+  },
+  {
+    "revision": "b485c81d901f388fe1b1",
+    "url": "/CIS_KMUTNB_bracket/static/css/main.62b07e60.chunk.css"
+  },
+  {
+    "revision": "033eb64aee7c8a118f16",
+    "url": "/CIS_KMUTNB_bracket/static/js/2.00123168.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/CIS_KMUTNB_bracket/static/js/2.00123168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b485c81d901f388fe1b1",
+    "url": "/CIS_KMUTNB_bracket/static/js/main.573fd4f7.chunk.js"
+  },
+  {
+    "revision": "fd968a4c547af547ca13",
+    "url": "/CIS_KMUTNB_bracket/static/js/runtime-main.5c896fb7.js"
+  }
+]);
